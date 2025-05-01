6d15a9b883a6924c004d3b32f4efea7c757537df
Ozzy <tacojrjr@outlook.com>
2025-05-01 17:33:59 -0300

    More work on spell equip menu

--- Scripts/menus/spell_menu.gd
+++ Scripts/menus/spell_menu.gd
@@ -1,6 +1,7 @@
 extends Control
 var active : bool = true
 var player
+@onready var select_slots = $"Selection/Select Slots".get_children()
 #Formatting: sprite path, cost, cast speed, description
 var spelltable_dict = {
 	"fireball" : ["res://icon.svg", 8, 0.3, "A small fireball, dealing minimal damage for modeste amounts of spirit."],
@@ -34,9 +35,15 @@ func update_slots():
 	var slots = %"Equip Slots".get_children()
 	slots[0].update(spelltable_dict[Playerstatus.equipped_spells[0]][0])
 	slots[1].update(spelltable_dict[Playerstatus.equipped_spells[1]][0])
+	for i in select_slots:
+		if i.holding != "":
+			i.update((spelltable_dict[i.holding])[0])
+		else:
+			i.locked = true
+			i.update(i.locked_image)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func info_update(spell):
 	pass
 
 func _input(event: InputEvent) -> void:
@@ -44,3 +51,8 @@ func _input(event: InputEvent) -> void:
 		if Input.is_action_just_pressed("pause") and get_tree().paused == true and active == true:
 			await get_tree().process_frame
 			close()
+	if event is InputEventMouseButton:
+		if Input.is_action_pressed("left_click"):
+			for i in select_slots:
+				if i.mouse_inside and i.locked == false:
+					info_update(i.holding)
--- Scripts/menus/spell_menu_slot.gd
+++ Scripts/menus/spell_menu_slot.gd
@@ -2,6 +2,10 @@ extends Panel
 var mouse_inside : bool = false
 var right_click_menu = null
 var rmb_menu_has_mouse = false
+@export var select_slot : bool = true
+@export var holding : String
+var locked : bool = false
+var locked_image = "res://textures/grass32.png"
 
 func update(spell_in_slot):
 	%Sprite2D.texture = load(spell_in_slot)
