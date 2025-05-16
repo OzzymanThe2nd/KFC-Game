59372c8ad0701bc3ae0a937bfcc51abe97961c09
Ozzy <tacojrjr@outlook.com>
2025-05-15 21:15:24 -0300

    Warp screen less shitty

--- Scripts/warp_point.gd
+++ Scripts/warp_point.gd
@@ -5,12 +5,18 @@ var warpspots = []
 var selected_warpspot
 var warplevel1 = "res://Scenes/Levels/test.tscn"
 var warplevel2 = "res://Scenes/Levels/dogshit.tscn"
+var icons
+var unlocked_icons = ["res://textures/bricks256.png","res://textures/concrete256_3.png","res://textures/darkgrass.png","res://textures/dirt32.png","res://textures/bricks256.png","res://textures/concrete256_3.png","res://textures/darkgrass.png","res://textures/dirt32.png"]
+
+func _ready() -> void:
+	icons = [%TextureRect1, %TextureRect2, %TextureRect3, %TextureRect4, %TextureRect5, %TextureRect6, %TextureRect7, %TextureRect8]
 
 func interact_with_player(player_grab):
 	player = player_grab
 	player.pixelate_off()
 	player.show_hud(false)
 	player.show_interact_prompt(false)
+	update_visuals()
 	$Menu.visible = true
 	active = true
 	get_tree().paused = true
@@ -29,6 +35,13 @@ func close():
 	$Menu.visible = false
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	get_tree().paused = false
+
+func update_visuals():
+	var run = 0
+	for i in Playerstatus.warpspots_unlocked:
+		if i == true:
+			icons[run].texture = load(unlocked_icons[run])
+		run += 1
 
 func _on_button_1_pressed() -> void:
 	if Playerstatus.warpspots_unlocked[0] == true:
