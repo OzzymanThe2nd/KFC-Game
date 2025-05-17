0797a65421eb9cdae6c586a260788b6505358693
Ozzy <tacojrjr@outlook.com>
2025-05-17 13:03:59 -0300

    Skeleton movement fix

--- Scripts/Entities/debug_enemy.gd
+++ Scripts/Entities/debug_enemy.gd
@@ -4,10 +4,12 @@ var agro = false
 var health = 10
 var SPEED = 1
 var rotation_speed = 0.03
+@export var movespeed = 0.9
 var dead = false
 var dmg_num = preload("res://Scenes/Entities/dmg_num.tscn")
 var item_drop = preload("res://Scenes/Items/item_pickup.tscn")
 var rot_dir = "left"
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 signal death
 
 func _on_player_det_body_entered(body: Node3D) -> void:
@@ -22,14 +24,21 @@ func turn():
 	global_transform = Transform3D(Basis(wrotation), global_transform.origin)
 
 func _physics_process(delta: float) -> void:
+	var direction = ((self.transform.basis) * Vector3(0, 0, -1)).normalized()
+	if not is_on_floor() and not dead:
+		velocity.y -= gravity * 0.016
 	if player != null and agro == true and dead == false and not $AnimationPlayer.current_animation == "attack":
 		var look_pos = player.position
 		look_pos.y = self.position.y
 		turn()
 		if dead == false and not $AnimationPlayer.current_animation == "attack" and not $AnimationPlayer.current_animation == "bounceback" and global_position.distance_to(player.global_position) > 1.9 and global_position.distance_to(player.global_position) < 11:
-			velocity = (((self.transform.basis) * Vector3(0, 0, -1)).normalized()) * SPEED
+			#velocity = (((self.transform.basis) * Vector3(0, 0, -1)).normalized()) * SPEED 
+			velocity.x = movespeed * direction.x
+			velocity.z = movespeed * direction.z
 			$AnimationPlayer.play("walk")
-			move_and_slide()
+		else: 
+			velocity.x = 0
+			velocity.z = 0
 		if $TestArea.overlaps_body(player) and not $AnimationPlayer.current_animation == "attack" and not $AnimationPlayer.current_animation == "bounceback" and not $AnimationPlayer.current_animation == "death":
 			$AnimationPlayer.play("attack")
 			%"Player Det".enabled = true
@@ -41,6 +50,7 @@ func _physics_process(delta: float) -> void:
 		%"Player Det".enabled = false
 		%"Shield Det".enabled = false
 		player.take_damage(7, "slash")
+	move_and_slide()
 
 func bounceback():
 	%"Player Det".enabled = false
@@ -69,11 +79,15 @@ func damage(x):
 
 func _on_animation_player_animation_started(anim_name: StringName) -> void:
 	if anim_name == "death":
+		velocity.x = 0
+		velocity.z = 0
 		set_collision_layer_value(2, false)
 		$CollisionShape3D.disabled = true
 		%"Player Det".enabled = false
 		%"Shield Det".enabled = false
-		
+	if anim_name == "attack":
+		velocity.x = 0
+		velocity.z = 0
 
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
